75d20333dc599af79ab499e0f13d0b84603eb6e0
JesusOkay <45755425+JesusOkay@users.noreply.github.com>
2024-12-30 22:39:03 +0100

    Update bots.php

--- api/bots.php
+++ api/bots.php
@@ -11,11 +11,16 @@ $uuids = [
     "5d9b4e09-62eb-4467-ad06-4de73eb419fe",
     "a43bc927-60eb-4711-81c3-763d196646ae",
     "2cb676cf-8b59-4c9e-84cb-34383a18950d",
-    "43086a02-34be-4b17-afce-4c1795de9ad1",
+    "43086a02-34be-4b17-afce-4c1795de9ad1"
+    // Más UUIDs...
 ];
 
-// Calcular el índice automáticamente basado en la cantidad de ejecuciones (por ejemplo, usando tiempo)
-$current_index = (int)(time() / 60) % count($uuids);
+// Calcular el índice secuencial basado en el número de ejecuciones
+static $execution_count = 0; // Variable estática para mantener el contador
+$current_index = $execution_count % count($uuids); // Calcular el índice
+
+// Incrementar el contador (reinicia automáticamente en cada carga de PHP)
+$execution_count++;
 
 // Seleccionar la UUID en orden
 $selected_uuid = $uuids[$current_index];
@@ -29,3 +34,4 @@ if ($response === FALSE) {
     echo $response . "\n";
 }
 ?>
+
